71bbbcb1990c770f0367fd6750b85adaab82d640
shigemk2 <i.am.shige@gmail.com>
2016-10-30 18:51:33 +0900

    Add ClaimCheck

--- src/main/scala/com/example/ClaimCheck.scala
+++ src/main/scala/com/example/ClaimCheck.scala
@@ -1,6 +1,16 @@
 package com.example
 
+import java.util.UUID
+
 import akka.actor._
 
 object ClaimCheckDriver extends CompletableApp(5) {
+}
+
+case class ClaimCheck() {
+  val number = UUID.randomUUID().toString
+
+  override def toString = {
+    "ClaimCheck(" + number + ")"
+  }
 }
