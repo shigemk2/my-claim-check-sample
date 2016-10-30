5c26c8bd7ec7e1b61e4fb1a8e2cdadbaba4d10e4
shigemk2 <i.am.shige@gmail.com>
2016-10-30 19:11:02 +0900

    Add ItemChecker

--- src/main/scala/com/example/ClaimCheck.scala
+++ src/main/scala/com/example/ClaimCheck.scala
@@ -17,3 +17,31 @@ case class ClaimCheck() {
 
 case class CheckedItem(claimCheck: ClaimCheck, businessId: String, parts: Map[String, Any])
 case class CheckedPart(claimCheck: ClaimCheck, part: Any)
+
+class ItemChecker {
+  val checkedItems = scala.collection.mutable.Map[ClaimCheck, CheckedItem]()
+
+  def checkedItemFor(businessId: String, parts: Map[String, Any]) = {
+    CheckedItem(ClaimCheck(), businessId, parts)
+  }
+
+  def checkItem(item: CheckedItem) = {
+    checkedItems.update(item.claimCheck, item)
+  }
+
+  def claimItem(claimCheck: ClaimCheck) = {
+    checkedItems(claimCheck)
+  }
+
+  def claimPart(claimCheck: ClaimCheck, partName: String): CheckedPart = {
+    val checkedItem = checkedItems(claimCheck)
+
+    CheckedPart(claimCheck, checkedItem.parts(partName))
+  }
+
+  def removeItem(claimCheck: ClaimCheck) = {
+    if (checkedItems.contains(claimCheck)) {
+      checkedItems.remove(claimCheck)
+    }
+  }
+}
